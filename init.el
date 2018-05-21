168e7c7ccf55ff3c6d4aa2c5f2ef10ef83e47769
torin <SecretSword2@users.noreply.github.com>
2018-05-21 21:03:20 +0900

    [add] autoinstall packages >> emacs

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
-; MELPA BEGIN
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-		    0  (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
+
+(load "~/.emacs.d/init-packages")
+; MELPA BEGIN
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,7 +14,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("57d7e8b7b7e0a22dc07357f0c30d18b33ffcbb7bcd9013ab2c9f70748cfa4838" default))))
+    ("9fe1540491fcf692b8c639a3abacd32b29233bc4cb834a12a0fd1e01cbd0a128" "57d7e8b7b7e0a22dc07357f0c30d18b33ffcbb7bcd9013ab2c9f70748cfa4838" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -27,10 +27,13 @@
 ;; Show numbers
 (global-linum-mode t)
 ;; Theme
-(load-theme 'desert)
+(load-theme 'cyberpunk)
 ;; Disable welocme screen
 (setq inhibit-startup-screen t)
 ;; Set tab width
 (setq tab-width 2)
 (defvaralias 'c-basic-offset 'tab-width)
 (defvaralias 'cperl-indent-level 'tab-width)
+;; Autocomplete
+(require 'auto-complete)
+(global-auto-complete-mode t)
