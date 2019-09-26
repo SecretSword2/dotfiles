5d4730c1893f6108db167743199ef776db402917
torin <SecretSword2@users.noreply.github.com>
2019-09-26 17:31:38 +0900

    refactor

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (setq custom-file (locate-user-emacs-file "custom"))
 
 (setq gc-cons-threshold (eval-when-compile (* 1024 1024 1024)))
-(run-with-idle-timer 2 t (lambda () (garbage-collect)))
+(run-with-idle-timer 2 t (lambda ()
+													 (garbage-collect)))
 
 (if (<= emacs-major-version 26)
 		(global-display-line-numbers-mode)
@@ -12,14 +13,22 @@
 
 (setq-default tab-width 2)
 (setq-default indent-tabs-mode t)
-(defvaralias 'c-basic-offset 'tab-width)
-(defvaralias 'cperl-indent-level 'tab-width)
+(defvaralias
+	'c-basic-offset
+	'tab-width
+	)
+(defvaralias
+	'cperl-indent-level
+	'tab-width
+	)
 
 (electric-indent-mode t)
 (electric-pair-mode t)
 (show-paren-mode 1)
 
-(set-face-attribute 'default nil :family "Cica" :height (if (eq system-type 'darwin) 170 140))
+(set-face-attribute 'default nil
+										:family "Cica"
+										:height (if (eq system-type 'darwin) 170 140))
 
 (set-language-environment "Japanese")
 
@@ -34,50 +43,64 @@
 
 ;; straight.el
 (defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(let ((bootstrap-file (expand-file-name
+											 "straight/repos/straight.el/bootstrap.el"
+											 user-emacs-directory))
+			(bootstrap-version 5))
+	(unless (file-exists-p bootstrap-file)
+		(with-current-buffer
+				(url-retrieve-synchronously
+				 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+				 'silent
+				 'inhibit-cookies)
+			(goto-char (point-max))
+			(eval-print-last-sexp)))
+	(load
+	 bootstrap-file
+	 nil
+	 'nomessage))
 
 (straight-use-package 'use-package)
 
-;; Themes
-(use-package dracula-theme
+;; Look and feel
+
+(use-package all-the-icons
 	:straight t
-	)
+	:custom
+	(all-the-icons-scale-factor 0.83))
+
+(use-package delight
+	:straight t)
+
+(use-package dracula-theme
+	:straight t)
 
 (use-package kaolin-themes
 	:straight t
 	:init
-	(load-theme 'kaolin-aurora t)
-	)
-
-(use-package all-the-icons
-	:straight t
-  :custom
-  (all-the-icons-scale-factor 0.83))
+	(load-theme 'kaolin-aurora t))
 
 ;; (use-package doom-modeline
 ;;   :straight t
 ;; 	:init
 ;; 	(doom-modeline-mode 1))
 
-(use-package projectile
+;; And everything else
+(use-package ace-window
 	:straight t
-  :config
-  (defun projectile-project-find-function (dir)
-    (let* ((root (projectile-project-root dir)))
-      (and root (cons 'transient root))))
-  (with-eval-after-load 'project
-    (add-to-list 'project-find-functions 'projectile-project-find-function))
-  )
+	:bind
+	("M-o" . ace-window))
+
+(use-package aggressive-indent
+	:straight t
+	:hook
+	(c++-mode . aggressive-indent-mode))
+
+(use-package anzu
+	:straight t
+	:delight
+	:config
+	(global-anzu-mode 1))
 
 (use-package company
 	:straight t
@@ -89,116 +112,96 @@
 		:custom
 		(company-lsp-async t)
 		(company-lsp-enable-snippet t)
-		(company-lsp-enable-recompletion t)
-		)
+		(company-lsp-enable-recompletion t))
 	:config
 	(global-company-mode)
 	(company-tng-configure-default)
 	:custom
-  (company-idle-delay 0)
-  (company-minimum-prefix-length 3)
+	(company-idle-delay 0)
+	(company-minimum-prefix-length 3)
 	(company-selection-wrap-around t)
 	(company-dabbrev-downcase nil))
 
-(use-package lsp-mode
-	:straight t
-	:init (use-package exec-path-from-shell
-					:straight t
-					:config
-					(when (memq window-system '(mac ns x))
-						(exec-path-from-shell-initialize)))
-	:hook
-	(before-save . lsp-format-buffer)
-	(c++-mode . lsp))
-
-(use-package lsp-ui
-	:straight t
-	)
-
-(use-package google-c-style
-	:straight (google-c-style :type git :host github :repo "google/styleguide" :branch "gh-pages")
-	:hook
-	(c++-mode . google-set-c-style)
-	(c-mode-common-hook . google-set-c-style)
-	(c++-mode-common-hook . google-set-c-style)
-	(c-mode-common-hook . google-make-newline-indent)
-	)
-
-(use-package aggressive-indent
-	:straight t
-	:hook
-	(c++-mode . aggressive-indent-mode)
-	)
-
-(use-package yasnippet
-	:delight yas-minor-mode
-	:straight t
-	:init
-	(yas-global-mode 1))
-
-(use-package yasnippet-snippets
-	:straight t
-	:requires yasnippet
-	:after yasnippet)
-
-(use-package hydra
-	:straight t)
-
 (use-package counsel
 	:straight t
-	:config
-  (counsel-mode 1))
-
-(use-package ivy
-	:straight t
-	:config
-	(setq ivy-use-virtual-buffers t)
-	(setq enable-recursive-minibuffers t)
-	(setq ivy-use-selectable-prompt t)
-	(ivy-mode 1)
+	:diminish
+	:init
+	(use-package amx
+		:straight t)
+	(use-package ivy
+		:straight t
+		:custom
+		(ivy-use-virtual-buffers t)
+		(enable-recursive-minibuffers t)
+		(ivy-use-selectable-prompt t)
+		:config
+		(ivy-mode 1)
+		:init
+		(use-package ivy-rich
+			:straight t
+			:requires ivy
+			:config
+			(ivy-rich-mode t)))
 	:bind
 	(("M-s M-s" . swiper)
 	 ("M-x" . counsel-M-x)
 	 ("M-y" . counsel-yank-pop)
 	 ("C-M-r" . counsel-recentf)
 	 ("C-x C-b" . counsel-ibuffer)
-	 ("C-M-f" . counsel-ag)))
-
-(use-package smex
-	:straight t
-	)
-
-(use-package ivy-hydra
-	:straight t
-	:after (ivy hydra)
-	:requires (ivy hydra))
-
-(use-package anzu
-	:straight t
-	:diminish anzu-mode
+	 ("C-M-f" . counsel-ag))
 	:config
-	(global-anzu-mode +1))
+	(counsel-mode 1))
+
+(use-package google-c-style
+	:straight (google-c-style
+						 :type git
+						 :host github
+						 :repo "google/styleguide"
+						 :branch "gh-pages")
+	:hook
+	(c++-mode . google-set-c-style)
+	(c-mode-common-hook . google-set-c-style)
+	(c++-mode-common-hook . google-set-c-style)
+	(c-mode-common-hook . google-make-newline-indent))
+
+(use-package lsp-mode
+	:straight t
+	:init
+	(use-package exec-path-from-shell
+		:straight t
+		:config (when (memq window-system '(mac ns x))
+							(exec-path-from-shell-initialize)))
+	(use-package lsp-ui
+		:straight t)
+	:hook
+	(before-save . lsp-format-buffer)
+	(c++-mode . lsp)
+	:custom
+	(lsp-auto-guess-root t))
+
+
+(use-package projectile
+	:straight t
+	:config
+	(defun projectile-project-find-function (dir)
+		(let* ((root (projectile-project-root dir)))
+			(and root
+					 (cons 'transient root))))
+	(with-eval-after-load
+			'project
+		(add-to-list
+		 'project-find-functions
+		 'projectile-project-find-function)))
 
 (use-package rainbow-delimiters
 	:straight t
-	:init
-	(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+	:hook
+	(prog-mode . rainbow-delimiters-mode)
 	:config
-	(set-face-attribute 'rainbow-delimiters-unmatched-face nil
+	(set-face-attribute	'rainbow-delimiters-unmatched-face nil
 											:foreground 'unspecified
 											:inherit 'error
 											:strike-through t))
-
-(use-package flycheck
-	:if (< emacs-major-version 26)
-	:straight t
-	:init
-	(global-flycheck-mode))
-
-(use-package flycheck-vale
-	:straight t
-	:init
-	(flycheck-vale-setup))
 
 (use-package undo-tree
 	:straight t
@@ -208,13 +211,16 @@
 (use-package web-mode
 	:mode ("\\.html?\\'" "\\.php\\'")
 	:config
-	(setq web-mode-markup-indent-offset 2)
+	(setq web-mode-markup-indent-offset	2)
 	(web-mode-use-tabs))
 
-(use-package markdown-mode
-	:straight t)
+(use-package yasnippet
+	:delight
+	:straight t
+	:init
+	(yas-global-mode 1))
 
-(use-package groovy-mode
-	:straight t)
-
-(provide 'init)
+(use-package yasnippet-snippets
+	:straight t
+	:requires yasnippet
+	:after yasnippet)
